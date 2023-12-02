6503edef6f1af8f809e50237f0ce6eb9dd4cf4a1
chrisstrom1 <108539898+chrisstrom1@users.noreply.github.com>
2023-12-02 15:10:21 -0600

    Update main.js
    2nd

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let express = require("express");
+let static = express.static("public")
+
 require("dotenv").config();
 let PORT = 8004;
 
 let app = express();
 app.use(express.json());
-
+app.use(static)
 let routes = require("./routes.js");
 
 app.use(routes);
@@ -12,4 +14,3 @@ app.use(routes);
 app.listen(PORT, function(){
     console.log("Application started on port", PORT);
 })
-
